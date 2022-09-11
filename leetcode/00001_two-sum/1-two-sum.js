1c739943717ec0ec49ebf261c504a6fdb95ce170
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2022-09-11 22:29:28 +0600

    Time: 57 ms (99.68%), Space: 43 MB (39.00%) - LeetHub

---
+++ leetcode/00001_two-sum/1-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const mp = new Map()
+    
+    for ( i = 0; i < nums.length; i++ ) {
+        if ( mp.has(target - nums[i]) ) {
+            return [i, mp.get(target - nums[i])] 
+        }
+        
+        mp.set(nums[i], i)
+    }
+    
+    return []
+};
