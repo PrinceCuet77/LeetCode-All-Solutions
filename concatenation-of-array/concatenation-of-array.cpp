8b210ef50ad3b8ee3eaa3d046b97fcf43e848cf4
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2023-06-22 12:20:56 +0600

    Time: 13 ms (38.90%) | Memory: 12.9 MB (13.19%) - LeetSync

--- concatenation-of-array/concatenation-of-array.cpp
+++ concatenation-of-array/concatenation-of-array.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
-        vector < int > result;
+  vector<int> getConcatenation(vector<int>& nums) {
+    vector < int > result;
 
-        for ( auto x : nums ) result.push_back(x);
-        for ( auto x : nums ) result.push_back(x);
-
-        return result;
+    for ( int i = 0; i < 2; i++ ) {
+      for ( auto value : nums ) {
+        result.push_back(value);
+      }
     }
+
+    return result;
+  }
 };
