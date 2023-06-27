cea292c35af439a006581a3b0707e695ac60d897
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2023-06-27 21:20:40 +0600

    Time: 27 ms (69.97%) | Memory: 10.6 MB (7.29%) - LeetSync

--- how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -3,14 +3,21 @@ public:
   vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
     vector < int > result;
     
+    map < int, int > mp;
     for ( int i = 0; i < nums.size(); i++ ) {
       int count = 0;
-      for ( int j = 0; j < nums.size(); j++ ) {
-        if ( i != j && nums[i] > nums[j] ) {
-          count++;
+      if ( mp[nums[i]] == 0 ) {
+        for ( int j = 0; j < nums.size(); j++ ) {
+          if ( i != j && nums[i] > nums[j] ) {
+            count++;
+          }
         }
+
+        result.push_back(count);
+        mp[nums[i]] = count;
+      } else {
+        result.push_back(mp[nums[i]]);
       }
-      result.push_back(count);
     }
 
     return result;
