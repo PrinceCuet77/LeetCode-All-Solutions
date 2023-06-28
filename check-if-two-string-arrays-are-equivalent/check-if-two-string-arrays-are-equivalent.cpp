6e77b6c5c520fb5850a1dadf49d0010f742cfd80
Rezoan Shakil Prince <52634555+PrinceCuet77@users.noreply.github.com>
2023-06-28 09:19:47 +0600

    Time: 0 ms (100.00%) | Memory: 11.4 MB (93.75%) - LeetSync

--- check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
   bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-    bool result = false;
+    int word1Index = 0, word2Index = 0, char1Index = 0, char2Index = 0;
 
-    string string1 = "", string2 = "";
-    int n1 = word1.size();
-    for ( int i = 0; i < n1; i++ ) {
-      string1 += word1[i];
+    while ( true ) {
+      char word1Char = word1[word1Index][char1Index];
+      char word2Char = word2[word2Index][char2Index];
+
+      if ( word1Char != word2Char ) { // If both character are not matched
+        return false;
+      }
+
+      char1Index++; // Incrementing the character index of current word from 'word1'
+      char2Index++; // Incrementing the character index of current word from 'word2'
+
+      if ( char1Index == word1[word1Index].size() ) { // If current word from 'word1' is over
+        word1Index++; // Move to next word of 'word1'
+        char1Index = 0; // Reset character index to 0 for start from beginning
+      }
+
+      if ( char2Index == word2[word2Index].size() ) { // If current word from 'word2' is over
+        word2Index++; // Move to next word of 'word1'
+        char2Index = 0; // Reset character index to 0 for start from beginning
+      }
+
+      if ( word1Index == word1.size() && word2Index == word2.size() ) { // Both words are ended
+        break;
+      }
+
+      // If 'word1' is finished and 'word2' is not finished then 'return false' and,
+      // 'word1' is not finished and 'word2' is not finished then 'return false'
+      if ( word1Index == word1.size() || word2Index == word2.size() ) {
+        return false;
+      } 
     }
 
-    int n2 = word2.size();
-    for ( int i = 0; i < n2; i++ ) {
-      string2 += word2[i];
-    }
- 
-    if ( string1 == string2 ) {
-      result = true;
-    }
-
-    return result;
+    return true;
   }
 };
